1355e350db9ca621854200e1c0a635a4f9da5328
Victor Zhang <yeannylam@gmail.com>
2020-09-26 08:49:57 +0800

    Initial Commit

---
+++ Scratch Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scratch Demo
+//
+//  Created by Victor Zhang on 2020/9/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
